5e2929887e8b09ec9a3c584e54449f5f62e32371
thetejendra <98267464+thetejendra@users.noreply.github.com>
2025-08-04 08:54:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+# st.title("Hey! Tejendra Kanwar")
+# st.header("How are you doing")
+
+name = st.text_input("Enter your name: ")
+fname = st.text_input("Enter your father's name: ")
+mname = st.text_input("Enter your mother's name: ")
+add = st.text_area("Enter your address :")
+
+button = st.button("Submit")
+
+# to show input
+# if button:
+#     st.markdown(f"""
+#     Name : {name}
+#     Father Name :{fname}
+#     Mother Name :{mname}
+#     Address : {add}
+# """)
